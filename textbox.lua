a78bd7f4fbde890303bf08fc94ce225a64595e34
roomunrecalled <yhorng@zoho.com>
2021-08-05 04:09:57 -0500

    refactoring - part2
    factored out a lot of draw() into helper functions
    actually you could probably just say a lot was stuffed into helper
    functions. cleaner looking? on the surface!

--- textbox.lua
+++ textbox.lua
@@ -10,6 +10,8 @@ local DEFAULT_BG_COLOR = {29,43,83}
 local DEFAULT_FG_COLOR = {255,241,232}
 local DEFAULT_TEXT_COLORS = {{0,0,0}, {41,173,255}, {255,108,36}}
 
+local TEXTBOX_MARGIN = {left = 4, right = 2}
+
 local SMALL_FONT_PATH = "assets/fonts/Poco/Poco.ttf"
 local SMALL_FONT_PADDING = {left = 8, top = 0, bottom = 6, right = 4}
 local SMALL_FONT_SIZE = 10
@@ -20,6 +22,7 @@ local LARGE_FONT_SIZE = 16
 local LARGE_FONT_HEIGHT = 0.9
 
 local BORDER_PATH = "assets/graphics/textbox_border.png"
+local ADVANCE_SYMBOL_PATH = ""
 
 local SMASH_SFX_PATH = ""
 local SHOT_SFX_PATH = "assets/sound/xenonn__layered-gunshot-9-edited.wav"
@@ -32,9 +35,6 @@ local ADVANCE_SFX_PATH = "assets/sound/yottasounds__typewriter-single-key-type-3
 local SCREEN_HEIGHT = 144
 local SCREEN_WIDTH = 192
 
-local tb_screen_dim = {height = SCREEN_HEIGHT, width = SCREEN_WIDTH}
-local update_tb__,tb_shader__
-
 local tb__ = {
 	current__ = nil,
 	coroutine__ = nil,
@@ -42,81 +42,25 @@ local tb__ = {
 }
 
 -- internal setup
+local init_tb_defaults__
 function tb__:init()
-	-- pre-process sounds
-	local default_sound = {
-			exclamation = love.audio.newSource(SHOT_SFX_PATH, "static"),
-			question = love.audio.newSource(CHORD_SFX_PATH, "static"),
-			lightbulb = love.audio.newSource(CORRECT_SFX_PATH, "static"),
-			chime = love.audio.newSource(CHIME_SFX_PATH, "static"),
-			text = love.audio.newSource(TEXT_SFX_PATH, "static"),
-			advance = love.audio.newSource(ADVANCE_SFX_PATH, "static")
-	}
+	local tb_screen_dim = {height = SCREEN_HEIGHT, width = SCREEN_WIDTH}
+	local update_tb__
+	local tb_shader__
 
-	for k,v in pairs(default_sound) do
-		v:setVolume(0.5)
-	end
-
-	-- pre-process default colors
-	local function convertColor(color_table)
-		local result = {}
-		for k,v in pairs(color_table) do
-			result[k] = v/255
-		end
-		return result
-	end
-
-	local default_color = {
-		bg = DEFAULT_BG_COLOR,
-		fg = DEFAULT_FG_COLOR,
-		text = DEFAULT_TEXT_COLORS,
-	}
-	default_color.bg = convertColor(default_color.bg)
-	default_color.fg = convertColor(default_color.fg)
-	for k,v in pairs(default_color.text) do
-		default_color.text[k] = convertColor(v)
-	end
-
-	local default_font = {
-		small = {
-			font = love.graphics.newFont(SMALL_FONT_PATH, SMALL_FONT_SIZE),
-			padding = SMALL_FONT_PADDING
-		},
-		large = {
-			font = love.graphics.newFont(LARGE_FONT_PATH, LARGE_FONT_SIZE),
-			padding = LARGE_FONT_PADDING
-		}
-	}
-	default_font.small.font:setLineHeight(SMALL_FONT_HEIGHT)
-	default_font.small.text_height =
-		math.floor(default_font.small.font:getHeight() *
-							 default_font.small.font:getLineHeight())
-	default_font.large.font:setLineHeight(LARGE_FONT_HEIGHT)
-	default_font.large.text_height =
-		math.floor(default_font.large.font:getHeight() * 
-							 default_font.large.font:getLineHeight())
-
+	init_tb_defaults__(self)
 	-- initialize TextboxString class
-	tb__.TextboxString = {
-		colored_text = default_color.text_color,
+	self.TextboxString = {
+		colored_text = self.default_color.text_color,
 		line_speed = 3,
 		line_pause = 3,
-		screen_shake = false,
-		screen_flash = false,
-		sound = nil
+		screen_shake = "none",
+		screen_flash = "none",
+		sound = "none",
+		effect = "none"
 	}
 
-	-- define 'pause' function
-	local function pause__(pause_length)
-		local elapsed_time, pause_time = 0, pause_length * .033
-
-		while elapsed_time < pause_time do
-			local time = coroutine.yield()
-			elapsed_time = elapsed_time + time
-		end
-	end
-
-	function tb__.TextboxString:new(string, options)
+	function self.TextboxString:new(string, options)
 		options = options or {}
 
 		-- process string and set flags
@@ -142,16 +86,16 @@ function tb__:init()
 		return options
 	end
 
-	tb__.Textbox = {
-		bg_color = default_color.bg,
-		fg_color = default_color.fg,
-		font = default_font,
-		sound = default_sound,
+	self.Textbox = {
+		bg_color = self.default_color.bg,
+		fg_color = self.default_color.fg,
+		font = self.default_font,
+		sound = self.default_sound,
 		line_speed = 3,
 		line_pause = 3,
 		placement = "bottom",
 		small_text = true,
-		text_color = default_color.text,
+		text_color = self.default_color.text,
 
 		strings = {},
 
@@ -160,24 +104,24 @@ function tb__:init()
 		mask = nil
 	}
 
-	function tb__.Textbox:new(o)
+	function self.Textbox:new(o)
 		o = o or {}
 		setmetatable(o, self)
 		self.__index = self
 		return o
 	end
 
-	function tb__:queue_length()
+	function self:queue_length()
 		return #self.queue__
 	end
 
-	function tb__:queue(strings, options)
+	function self:queue(strings, options)
 		local new_textbox = options or {}
 		new_textbox.strings = strings
 		table.insert(self.queue__, self.Textbox:new(new_textbox))
 	end
 
-	function tb__:run(dt, close_textbox)
+	function self:run(dt, close_textbox)
 		local ok, message
 		if not self.coroutine__ then
 			if #self.queue__ > 0 then
@@ -198,52 +142,22 @@ function tb__:init()
 		end
 	end
 
-	local function draw_textbox()
-	end
-	function tb__:draw(screencanvas, textcanvas)
+	local draw_textbox__, print_text__, draw_mask__
+	function self:draw(screencanvas, textcanvas)
 		local current = self.current__
 		local old_color = {love.graphics.getColor()}
 		if current and current.mask then
 			-- draw the textbox
-			love.graphics.setColor(current.bg_color)
-			love.graphics.rectangle("fill",
-				0,
-				current.dimensions.top,
-				tb_screen_dim.width,
-				current.dimensions.height)
+			draw_textbox__(current)
 
-			love.graphics.setColor(current.fg_color)
-			love.graphics.rectangle("fill",
-				2,
-				current.dimensions.top + 2,
-				tb_screen_dim.width - 4,
-				current.dimensions.height - 4)
-
-			local oldFont = love.graphics.getFont()
 			-- print the text
-			love.graphics.setColor(current.text_color[1])
 			local current_font =
 				current.small_text and current.font.small or current.font.large
-			love.graphics.setFont(current_font.font)
-			for k,v in pairs(current.strings) do
-				love.graphics.printf(
-					v, 
-					current_font.padding.left,
-					current.dimensions.top + current_font.padding.top + (k-1)*current_font.text_height,
-					tb_screen_dim.width - 4)
-			end
-			love.graphics.setFont(oldFont)
+			print_text__(current, current_font)
 
 			-- draw the mask
-			if self.current__.mask > 0 then
-				love.graphics.setColor(current.fg_color)
-				love.graphics.rectangle(
-					"fill",
-					current_font.padding.left,
-					current.dimensions.top + current.dimensions.height - current_font.padding.bottom + 1,
-					tb_screen_dim.width - (current_font.padding.left + current_font.padding.right),
-					-current.mask)
-			end
+			draw_mask__(current, current_font)
+
 			-- draw the 'continue' symbol
 			if self.current__.completed then
 				love.graphics.setColor(current.text_color[1])
@@ -256,9 +170,10 @@ function tb__:init()
 		end
 	end
 
+	local tb_pause__, display_line__
 	update_tb__ = function ()
 		local tb_close = false
-		local current = tb__.current__
+		local current = self.current__
 
 		local at_bottom = current.placement == "bottom"
 		local current_font =
@@ -273,56 +188,161 @@ function tb__:init()
 		local line_speed = LINE_SPEED[current.line_speed]
 		local line_pause = LINE_PAUSE[current.line_pause]
 
-		-- load sounds
-		local text_sound = current.sound.text
-		local ending_sound = current.sound.text
-
-		pause__(4)
+		tb_pause__(4)
 
 		current.mask = text_height * 2
-		while current.mask > text_height do
-			current.mask = current.mask - 1
-			if current.mask == math.floor(text_height * 1.5) then
-				text_sound:stop()
-				text_sound:play()
-			end
-			pause__(line_speed)
-		end
 
-		ending_sound:stop()
-		--ending_sound:play()
-		text_sound:play()
+		-- display the first line
+		display_line__(current, nil, text_height * 2, text_height)
 
-		pause__(line_pause)
+		tb_pause__(line_pause)
 		if current.line_pause > 3 then
 			current.mask = current.mask - (current.line_pause - 2)
 		end
 
+		-- display the second line
 		if (#current.strings > 1) then
-			while current.mask > 0 do
-				current.mask = current.mask - 1
-				if current.mask == math.floor(text_height * 0.5) then
-					text_sound:stop()
-					text_sound:play()
-				end
-				pause__(line_speed)
-			end
+			display_line__(current, nil, text_height, 0)
 		end
 
-		ending_sound:stop()
-		ending_sound:play()
-
-		pause__(16)
+		tb_pause__(16)
 		current.completed = true
 
 		while not tb_close do
 			_, tb_close = coroutine.yield()
 		end
 
-		pause__(2)
+		tb_pause__(2)
 
 		current.sound.advance:play()
 	end
+
+	-- tb:draw() helper functions
+	draw_textbox__ = function(textbox)
+		love.graphics.setColor(textbox.bg_color)
+		love.graphics.rectangle("fill",
+			TEXTBOX_MARGIN.left,
+			textbox.dimensions.top,
+			tb_screen_dim.width - (TEXTBOX_MARGIN.left + TEXTBOX_MARGIN.right),
+			textbox.dimensions.height)
+
+		love.graphics.setColor(textbox.fg_color)
+		-- this is supposed to be replaced with a sprite, so magic numbers are fine
+		love.graphics.rectangle("fill",
+			TEXTBOX_MARGIN.left + 2,
+			textbox.dimensions.top + 2,
+			tb_screen_dim.width - (TEXTBOX_MARGIN.left + TEXTBOX_MARGIN.right) - 4,
+			textbox.dimensions.height - 4)
+	end
+	print_text__ = function(textbox, current_font)
+		local oldFont = love.graphics.getFont()
+		love.graphics.setFont(current_font.font)
+		love.graphics.setColor(textbox.text_color[1])
+		for k,v in pairs(textbox.strings) do
+			love.graphics.printf(
+				v, 
+				TEXTBOX_MARGIN.left + current_font.padding.left,
+				textbox.dimensions.top + current_font.padding.top + (k-1)*current_font.text_height,
+				tb_screen_dim.width - 4)
+		end
+		love.graphics.setFont(oldFont)
+	end
+	draw_mask__ = function(textbox, current_font)
+		if textbox.mask > 0 then
+			love.graphics.setColor(textbox.fg_color)
+			love.graphics.rectangle(
+				"fill",
+				TEXTBOX_MARGIN.left + current_font.padding.left,
+				textbox.dimensions.top + textbox.dimensions.height - current_font.padding.bottom + 2,
+				tb_screen_dim.width -
+					(TEXTBOX_MARGIN.left + TEXTBOX_MARGIN.right) -
+					(current_font.padding.left + current_font.padding.right),
+				-textbox.mask)
+		end
+	end
+
+	-- update_tb__ helper functions
+	tb_pause__ = function(pause_length)
+		local elapsed_time, pause_time = 0, pause_length * .033
+
+		while elapsed_time < pause_time do
+			local time = coroutine.yield()
+			elapsed_time = elapsed_time + time
+		end
+	end
+
+	display_line__ = function(textbox, line, max, min)
+		local text_sfx = textbox.sound.text
+		local sfx = textbox.sound.text
+		while textbox.mask > min do
+			textbox.mask = textbox.mask - 1
+			if textbox.mask == math.floor((max + min) * 0.52) then
+				text_sfx:stop()
+				text_sfx:play()
+			end
+			-- TODO: change to line
+			tb_pause__(LINE_SPEED[textbox.line_speed])
+		end
+
+		sfx:stop()
+		sfx:play()
+	end
+end
+
+-- init function for tb__. Yes this is messy.
+init_tb_defaults__ = function(self)
+	-- load and pre-process sounds
+	self.default_sound = {
+			exclamation = love.audio.newSource(SHOT_SFX_PATH, "static"),
+			question = love.audio.newSource(CHORD_SFX_PATH, "static"),
+			lightbulb = love.audio.newSource(CORRECT_SFX_PATH, "static"),
+			chime = love.audio.newSource(CHIME_SFX_PATH, "static"),
+			text = love.audio.newSource(TEXT_SFX_PATH, "static"),
+			advance = love.audio.newSource(ADVANCE_SFX_PATH, "static")
+	}
+
+	for k,v in pairs(self.default_sound) do
+		v:setVolume(0.5)
+	end
+
+	-- pre-process default colors
+	local function convertColor(color_table)
+		local result = {}
+		for k,v in pairs(color_table) do
+			result[k] = v/255
+		end
+		return result
+	end
+
+	self.default_color = {
+		bg = DEFAULT_BG_COLOR,
+		fg = DEFAULT_FG_COLOR,
+		text = DEFAULT_TEXT_COLORS,
+	}
+	self.default_color.bg = convertColor(self.default_color.bg)
+	self.default_color.fg = convertColor(self.default_color.fg)
+	for k,v in pairs(self.default_color.text) do
+		self.default_color.text[k] = convertColor(v)
+	end
+
+	self.default_font = {
+		small = {
+			font = love.graphics.newFont(SMALL_FONT_PATH, SMALL_FONT_SIZE),
+			padding = SMALL_FONT_PADDING
+		},
+		large = {
+			font = love.graphics.newFont(LARGE_FONT_PATH, LARGE_FONT_SIZE),
+			padding = LARGE_FONT_PADDING
+		}
+	}
+	self.default_font.small.font:setLineHeight(SMALL_FONT_HEIGHT)
+	self.default_font.small.text_height =
+		math.floor(self.default_font.small.font:getHeight() *
+								self.default_font.small.font:getLineHeight())
+	self.default_font.large.font:setLineHeight(LARGE_FONT_HEIGHT)
+	self.default_font.large.text_height =
+		math.floor(self.default_font.large.font:getHeight() *
+								self.default_font.large.font:getLineHeight())
 end
 
 return tb__
